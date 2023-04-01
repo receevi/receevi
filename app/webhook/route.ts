0634aaa84982f98c1ef22bb6cd2937fdbf09b2ba
Juned Khatri <juned.khatri31@gmail.com>
2023-04-01 14:46:47 +0530

    Add message receiver

--- app/webhook/route.ts
+++ app/webhook/route.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { headers } from 'next/headers';
+import { verifyWebhook } from '../../lib/verify';
+import { WebHookRequest } from '../../types/webhook';
+import { createServiceClient } from '../../lib/supabase/service-client';
 
 export async function GET(request: Request) {
   const urlDecoded = new URL(request.url)
@@ -19,5 +23,22 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: NextRequest) {
-  const requestBody = await request.json()
+  const headersList = headers();
+  const xHubSigrature256 = headersList.get('x-hub-signature-256');
+  const rawRequestBody = await request.text()
+  console.log('rawRequestBody', rawRequestBody)
+  if (!xHubSigrature256 || !verifyWebhook(rawRequestBody, xHubSigrature256)) {
+    return new NextResponse(null, { status: 401 })
+  }
+  const webhookBody = JSON.parse(rawRequestBody) as WebHookRequest;
+  if (webhookBody.entry.length > 0) {
+    const supabase = createServiceClient()
+    let { error } = await supabase
+        .from('webhook')
+        .insert(webhookBody.entry.map((entry) => {
+          return { payload: entry }
+        }))
+    if (error) throw error
+  }
+  return new NextResponse()
 }
---
+++ types/webhook.d.ts
@@ -0,0 +1,21 @@
+export type WebHookRequest = {
+    object: "whatsapp_business_account",
+    entry: [
+        {
+            id: string,
+            changes: [
+                {
+                    value: {
+                        metadata: {
+                            display_phone_number: string,
+                            phone_number_id: string,
+                        }
+                        contacts: Contact[],
+                        messages: Message[],
+                    },
+                    field: string
+                }
+            ]
+        }
+    ]
+}
