948822e855b1c2f559a7fa9559a6ddd6cdd1cec2
Juned Khatri <juned.khatri31@gmail.com>
2023-04-01 22:14:27 +0530

    Add messages to database

--- app/webhook/route.ts
+++ app/webhook/route.ts
@@ -3,6 +3,9 @@ import { headers } from 'next/headers';
 import { verifyWebhook } from '../../lib/verify';
 import { WebHookRequest } from '../../types/webhook';
 import { createServiceClient } from '../../lib/supabase/service-client';
+import { DBTables } from '../../enums/Tables';
+
+export const revalidate = 0
 
 export async function GET(request: Request) {
   const urlDecoded = new URL(request.url)
@@ -34,11 +37,41 @@ export async function POST(request: NextRequest) {
   if (webhookBody.entry.length > 0) {
     const supabase = createServiceClient()
     let { error } = await supabase
-        .from('webhook')
-        .insert(webhookBody.entry.map((entry) => {
-          return { payload: entry }
-        }))
+      .from(DBTables.Webhook)
+      .insert(webhookBody.entry.map((entry) => {
+        return { payload: entry }
+      }))
     if (error) throw error
+    const changes = webhookBody.entry[0].changes;
+    if (changes.length > 0) {
+      if (changes[0].field === "messages") {
+        const changeValue = changes[0].value;
+        const contacts = changeValue.contacts;
+        const messages = changeValue.messages;
+        if (contacts && contacts.length > 0) {
+          for (const contact of contacts) {
+            let { error } = await supabase
+              .from(DBTables.Contacts)
+              .upsert({
+                wa_id: contact.wa_id,
+                profile_name: contact.profile.name,
+              })
+            if (error) throw error
+          }
+        }
+        if (messages) {
+          let { error } = await supabase
+            .from(DBTables.Messages)
+            .insert(messages.map(message => {
+              return {
+                from: message.from,
+                message: message
+              }
+            }))
+          if (error) throw error
+        }
+      }
+    }
   }
   return new NextResponse()
 }
