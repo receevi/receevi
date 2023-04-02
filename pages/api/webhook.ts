08193d78e3e915de675768775bc41ac06b7a946a
Juned Khatri <juned.khatri31@gmail.com>
2023-04-02 14:20:27 +0530

    Remove unused function

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,31 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { DBCollection } from '../../enums/DBCollections'
-import clientPromise from '../../lib/mongodb'
-import { verifyWebhook } from '../../lib/verify'
-import { Contact } from '../../types/contact'
-import { Message } from '../../types/Message'
-
-type WebHookRequest = {
-    object: "whatsapp_business_account",
-    entry: [
-        {
-            id: string,
-            changes: [
-                {
-                    value: {
-                        metadata: {
-                            display_phone_number: string,
-                            phone_number_id: string,
-                        }
-                        contacts: Contact[],
-                        messages: Message[],
-                    },
-                    field: string
-                }
-            ]
-        }
-    ]
-}
 
 export default async function handler(
     req: NextApiRequest,
