403d3fcd4795b31c86d8b080c00fdf7e2a425b76
Juned Khatri <juned.khatri31@gmail.com>
2023-04-04 12:55:20 +0530

    Add scroll and scroll to bottom on new message

--- app/chats/[wa_id]/MessageListClient.tsx
+++ app/chats/[wa_id]/MessageListClient.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { DBTables } from "../../../enums/Tables"
 import { MessageJson, TextMessage } from "../../../types/Message"
 import { createClient } from "../../../utils/supabase-browser"
@@ -10,7 +10,14 @@ import ReceivedTextMessageUI from "./ReceivedTextMessageUI"
 export default function MessageListClient({ messages, from }: { messages: DBMessage[], from: string }) {
     const [supabase] = useState(() => createClient())
     const [stateMessages, setMessages ] = useState<DBMessage[]>(messages)
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+        }
+    }
     useEffect(() => {
+        scrollToBottom()
         const channel = supabase
             .channel('any')
             .on<DBMessage>('postgres_changes', {
@@ -20,12 +27,13 @@ export default function MessageListClient({ messages, from }: { messages: DBMess
                 filter: `from=eq.${from}`
             }, payload => {
                 setMessages([...stateMessages, payload.new])
+                scrollToBottom()
             })
             .subscribe()
         return () => { supabase.removeChannel(channel) }
     })
     return (
-        <div className="px-16 py-2">
+        <div className="px-16 py-2 h-full overflow-y-auto" ref={messagesEndRef}>
             {stateMessages.map((message) => {
                 const messageBody = message.message as MessageJson
                 return (
