c66b1cde30e954d72a9cb42fd0d011c0b897fda1
Juned Khatri <juned.khatri31@gmail.com>
2023-04-04 13:21:59 +0530

    chat background same as whatsapp web

--- app/chats/[wa_id]/page.tsx
+++ app/chats/[wa_id]/page.tsx
@@ -12,8 +12,9 @@ export default async function ContactChat({ params }: { params: { wa_id: string 
         .eq('from', params.wa_id)
     if (error) throw error
     return (
-        <div className="bg-conversation-panel-background h-full">
-            <div className="bg-chat-img h-full">
+        <div className="bg-conversation-panel-background h-full relative">
+            <div className="bg-chat-img h-full w-full absolute bg-[length:412.5px_749.25px] opacity-40"></div>
+            <div className="h-full relative">
                 {
                     (() => {
                         if (messages != null) {
