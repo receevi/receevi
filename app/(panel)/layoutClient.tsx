2b4bbab869299ee527aa780f24b06e5a0bf0879d
Juned Khatri <juned.khatri31@gmail.com>
2023-10-21 12:47:10 +0530

    Fix in chat false (#1)
    * Fix menu highlight not working
    
    * Fix in chat false on new message

--- app/(panel)/layoutClient.tsx
+++ app/(panel)/layoutClient.tsx
@@ -12,9 +12,9 @@ export default function PanelClient({ children }: { children: ReactNode }) {
             <div className="flex-[6] border-e-2 border-e-slate-100 p-4">
                 <div className="text-center">Whatsapp Webhook</div>
                 <div className="mt-8">
-                    <Link href="/chats"><Button variant={activePath === '/chats' ? "default" : "ghost"} className="w-full justify-start">Chats</Button></Link>
-                    <Link href="/contacts"><Button variant={activePath === '/contacts' ? "default" : "ghost"} className="w-full justify-start">Contacts</Button></Link>
-                    <Link href="/bulk-send"><Button variant={activePath === '/bulk-send' ? "default" : "ghost"} className="w-full justify-start">Bulk Send</Button></Link>
+                    <Link href="/chats"><Button variant={activePath?.startsWith('/chats')? "default" : "ghost"} className="w-full justify-start">Chats</Button></Link>
+                    <Link href="/contacts"><Button variant={activePath?.startsWith('/contacts') ? "default" : "ghost"} className="w-full justify-start">Contacts</Button></Link>
+                    <Link href="/bulk-send"><Button variant={activePath?.startsWith('/bulk-send') ? "default" : "ghost"} className="w-full justify-start">Bulk Send</Button></Link>
                 </div>
             </div>
             <div className="flex-[20] p-4">
