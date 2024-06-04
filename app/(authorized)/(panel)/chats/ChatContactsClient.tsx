387c98cf3dbc621236a04d5dedc52a0b9e27a04d
Juned Khatri <juned.khatri31@gmail.com>
2024-06-04 10:17:40 +0530

    fix: Subscribing immediately after unsubsribing not working

--- app/(authorized)/(panel)/chats/ChatContactsClient.tsx
+++ app/(authorized)/(panel)/chats/ChatContactsClient.tsx
@@ -5,49 +5,58 @@ import { DBTables } from "@/lib/enums/Tables";
 import { Contact } from "@/types/contact";
 import { createClient } from "@/utils/supabase-browser";
 import ContactUI from "./ContactUI";
+import { RealtimeChannel } from "@supabase/supabase-js";
 
 export default function ChatContactsClient({ contacts }: { contacts: Contact[] }) {
     const [supabase] = useState(() => createClient())
-    const [contactsState, setContacts ] = useState<Contact[]>(contacts)
+    const [contactsState, setContacts] = useState<Contact[]>(contacts)
     useEffect(() => {
-        const channel = supabase
-            .channel('any')
-            .on<Contact>('postgres_changes', { event: '*', schema: 'public', table: DBTables.Contacts }, payload => {
-                switch(payload.eventType) {
-                    case "INSERT":
-                        contactsState.splice(0, 0, payload.new)
-                        setContacts([...contactsState])
-                        break;
-                    case "UPDATE":
-                        const indexOfItem = contactsState.findIndex((contact: Contact) => contact.wa_id == payload.old.wa_id)
-                        if (indexOfItem !== -1) {
-                            contactsState[indexOfItem] = payload.new
-                            contactsState.sort((a: Contact, b: Contact) => {
-                                if (!a.last_message_at || !b.last_message_at) {
-                                    return 0;
-                                }
-                                const aDate = new Date(a.last_message_at)
-                                const bDate =  new Date(b.last_message_at)
-                                if (aDate > bDate) {
-                                    return -1;
-                                } else if (bDate > aDate) {
-                                    return 1;
-                                }
-                                return 0;
-                            })
+        let channel: RealtimeChannel | null = null;
+        const timeout = setTimeout(() => {
+            channel = supabase
+                .channel('any')
+                .on<Contact>('postgres_changes', { event: '*', schema: 'public', table: DBTables.Contacts }, payload => {
+                    switch (payload.eventType) {
+                        case "INSERT":
+                            contactsState.splice(0, 0, payload.new)
                             setContacts([...contactsState])
-                        } else {
-                            console.warn(`Could not find contact to update contact for id: ${payload.old.wa_id}`)
-                        }
-                        break;
-                    case "DELETE":
-                        const newContacts = contactsState.filter((item: Contact) => item.wa_id != payload.old.wa_id)
-                        setContacts(newContacts)
-                        break;
-                }
-            })
-            .subscribe()
-        return () => { supabase.removeChannel(channel) }
+                            break;
+                        case "UPDATE":
+                            const indexOfItem = contactsState.findIndex((contact: Contact) => contact.wa_id == payload.old.wa_id)
+                            if (indexOfItem !== -1) {
+                                contactsState[indexOfItem] = payload.new
+                                contactsState.sort((a: Contact, b: Contact) => {
+                                    if (!a.last_message_at || !b.last_message_at) {
+                                        return 0;
+                                    }
+                                    const aDate = new Date(a.last_message_at)
+                                    const bDate = new Date(b.last_message_at)
+                                    if (aDate > bDate) {
+                                        return -1;
+                                    } else if (bDate > aDate) {
+                                        return 1;
+                                    }
+                                    return 0;
+                                })
+                                setContacts([...contactsState])
+                            } else {
+                                console.warn(`Could not find contact to update contact for id: ${payload.old.wa_id}`)
+                            }
+                            break;
+                        case "DELETE":
+                            const newContacts = contactsState.filter((item: Contact) => item.wa_id != payload.old.wa_id)
+                            setContacts(newContacts)
+                            break;
+                    }
+                })
+                .subscribe()
+        }, 200)
+        return () => {
+            clearTimeout(timeout)
+            if (channel) {
+                supabase.removeChannel(channel);
+            }
+        }
     })
     return (
         <div className="flex flex-col">
