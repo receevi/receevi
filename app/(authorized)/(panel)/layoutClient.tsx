4135a759b001516cf415ce1b8a3ce41d9d9a81ba
Juned Khatri <juned.khatri31@gmail.com>
2024-07-02 14:02:01 +0530

    chore: Add icons to left panel

--- app/(authorized)/(panel)/layoutClient.tsx
+++ app/(authorized)/(panel)/layoutClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
+import { ContactIcon, MessageCircleIcon, RadioIcon } from "lucide-react";
 import Link from 'next/link';
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
@@ -9,12 +10,12 @@ export default function PanelClient({ children }: { children: ReactNode }) {
     const activePath = usePathname();
     return (
         <div className="flex flex-row h-screen">
-            <div className="flex-[4] border-e-2 border-e-slate-100 p-4">
+            <div className="flex-[6] max-w-xs border-e-2 border-e-slate-100 p-4">
                 <div className="text-center">Receevi</div>
                 <div className="mt-8">
-                    <Link href="/chats"><Button variant={activePath?.startsWith('/chats')? "default" : "ghost"} className="w-full justify-start">Chats</Button></Link>
-                    <Link href="/contacts"><Button variant={activePath?.startsWith('/contacts') ? "default" : "ghost"} className="w-full justify-start">Contacts</Button></Link>
-                    <Link href="/bulk-send"><Button variant={activePath?.startsWith('/bulk-send') ? "default" : "ghost"} className="w-full justify-start">Bulk Send</Button></Link>
+                    <Link href="/chats"><Button variant={activePath?.startsWith('/chats')? "secondary" : "ghost"} className="w-full justify-start"> <MessageCircleIcon/>&nbsp;&nbsp;Chats</Button></Link>
+                    <Link href="/contacts"><Button variant={activePath?.startsWith('/contacts') ? "secondary" : "ghost"} className="w-full justify-start"><ContactIcon/>&nbsp;&nbsp;Contacts</Button></Link>
+                    <Link href="/bulk-send"><Button variant={activePath?.startsWith('/bulk-send') ? "secondary" : "ghost"} className="w-full justify-start"> <RadioIcon/>&nbsp;&nbsp;Bulk Send</Button></Link>
                 </div>
             </div>
             <div className="flex-[20] p-4">
